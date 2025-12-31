f3425f8a996845b19538c9906329712fd88f58fb
pbbhattpriyanshu <bhattpriyanshupcm007@gmail.com>
2025-12-31 23:23:58 +0530

    Add initial implementation of date display using LocalDate

---
+++ index.java
@@ -0,0 +1,8 @@
+import java.time.LocalDate;
+
+public class index {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        System.out.println("Today's date is: " + today);
+    }
+}
